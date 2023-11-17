f6b98a05cf3925f74f15a510c86392d02824d1ba
Nacho <italiyankina@gmail.com>
2023-11-17 20:28:07 +0000

    random Modulo fixes

--- app/Http/Controllers/Api/V1/ModuloController.php
+++ app/Http/Controllers/Api/V1/ModuloController.php
@@ -1,19 +1,36 @@
 <?php
 
-namespace App\Http\Controllers\Api\V1;
+namespace App\Http\Controllers;
 
-use App\Http\Controllers\Controller;
 use App\Models\Modulo;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 
 class ModuloController extends Controller
 {
     /**
      * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
      */
     public function index()
     {
-        //
+        $Modulos = Modulo::latest()->get();
+        
+        if (is_null($Modulos->first())) {
+            return response()->json([
+                'status' => 'failed',
+                'message' => 'No Modulo found!',
+            ], 200);
+        }
+
+        $response = [
+            'status' => 'success',
+            'message' => 'Modulos are retrieved successfully.',
+            'data' => $Modulos,
+        ];
+
+        return response()->json($response, 200);
     }
 
     /**
@@ -21,30 +38,136 @@ class ModuloController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $validate = Validator::make($request->all(), [
+            'name' => 'required|string|max:250',
+            'description' => 'required|string|'
+        ]);
+
+        if($validate->fails()){  
+            return response()->json([
+                'status' => 'failed',
+                'message' => 'Validation Error!',
+                'data' => $validate->errors(),
+            ], 403);    
+        }
+
+        $Modulo = Modulo::create($request->all());
+
+        $response = [
+            'status' => 'success',
+            'message' => 'Modulo is added successfully.',
+            'data' => $Modulo,
+        ];
+
+        return response()->json($response, 200);
     }
 
     /**
      * Display the specified resource.
      */
-    public function show(Modulo $modulo)
+    public function show($id)
     {
-        //
+        $Modulo = Modulo::find($id);
+  
+        if (is_null($Modulo)) {
+            return response()->json([
+                'status' => 'failed',
+                'message' => 'Modulo is not found!',
+            ], 200);
+        }
+
+        $response = [
+            'status' => 'success',
+            'message' => 'Modulo is retrieved successfully.',
+            'data' => $Modulo,
+        ];
+        
+        return response()->json($response, 200);
     }
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, Modulo $modulo)
+    public function update(Request $request, $id)
     {
-        //
+        $validate = Validator::make($request->all(), [
+            'name' => 'required',
+            'description' => 'required'
+        ]);
+
+        if($validate->fails()){  
+            return response()->json([
+                'status' => 'failed',
+                'message' => 'Validation Error!',
+                'data' => $validate->errors(),
+            ], 403);
+        }
+
+        $Modulo = Modulo::find($id);
+
+        if (is_null($Modulo)) {
+            return response()->json([
+                'status' => 'failed',
+                'message' => 'Modulo is not found!',
+            ], 200);
+        }
+
+        $Modulo->update($request->all());
+        
+        $response = [
+            'status' => 'success',
+            'message' => 'Modulo is updated successfully.',
+            'data' => $Modulo,
+        ];
+
+        return response()->json($response, 200);
     }
 
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(Modulo $modulo)
+    public function destroy($id)
     {
-        //
+        $Modulo = Modulo::find($id);
+  
+        if (is_null($Modulo)) {
+            return response()->json([
+                'status' => 'failed',
+                'message' => 'Modulo is not found!',
+            ], 200);
+        }
+
+        Modulo::destroy($id);
+        return response()->json([
+            'status' => 'success',
+            'message' => 'Modulo is deleted successfully.'
+            ], 200);
+    }
+
+    /**
+     * Search by a Modulo name
+     *
+     * @param  str  $name
+     * @return \Illuminate\Http\Response
+     */
+    public function search($name)
+    {
+        $Modulos = Modulo::where('name', 'like', '%'.$name.'%')
+            ->latest()->get();
+
+        if (is_null($Modulos->first())) {
+            return response()->json([
+                'status' => 'failed',
+                'message' => 'No Modulo found!',
+            ], 200);
+        }
+
+        $response = [
+            'status' => 'success',
+            'message' => 'Modulos are retrieved successfully.',
+            'data' => $Modulos,
+        ];
+
+        return response()->json($response, 200);
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -3,7 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Auth\LoginRegisterController;
-use App\Http\Controllers\ProductController;
+use App\Http\Controllers\ModuloController;
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -23,21 +23,21 @@ Route::controller(LoginRegisterController::class)->group(function() {
     Route::post('/login', 'login');
 });
 
-// Public routes of product
-Route::controller(ProductController::class)->group(function() {
-    Route::get('/products', 'index');
-    Route::get('/products/{id}', 'show');
-    Route::get('/products/search/{name}', 'search');
+// Public routes of Modulo
+Route::controller(ModuloController::class)->group(function() {
+    Route::get('/modulos', 'index');
+    Route::get('/modulos/{id}', 'show');
+    Route::get('/modulos/search/{name}', 'search');
 });
 
-// Protected routes of product and logout
+// Protected routes of Modulo and logout
 Route::middleware('auth:sanctum')->group( function () {
     Route::post('/logout', [LoginRegisterController::class, 'logout']);
 
-    Route::controller(ProductController::class)->group(function() {
-        Route::post('/products', 'store');
-        Route::post('/products/{id}', 'update');
-        Route::delete('/products/{id}', 'destroy');
+    Route::controller(ModuloController::class)->group(function() {
+        Route::post('/modulos', 'store');
+        Route::post('/modulos/{id}', 'update');
+        Route::delete('/modulos/{id}', 'destroy');
     });
 });
 
